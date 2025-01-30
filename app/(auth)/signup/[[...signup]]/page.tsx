4d4227aada9e7d1f6cafa479462e1091cdb9fb1a
NSinecode <prikhodko.pk@gmail.com>
2025-01-30 18:00:58 +0300

    added alpha version of login

---
+++ app/(auth)/signup/[[...signup]]/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+import ClientOnly from "../../components/client-only";
+
+export default function SignUpPage() {
+  const { theme } = useTheme();
+
+  return (
+    <ClientOnly>
+      <SignUp
+        forceRedirectUrl="/todo"
+        appearance={{ baseTheme: theme === "dark" ? dark : undefined }}
+      />
+    </ClientOnly>
+  );
+}
