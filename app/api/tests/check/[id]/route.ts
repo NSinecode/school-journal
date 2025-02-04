65be8d67ae0592ba854e3b4e2eaec01474b4edf9
NSinecode <prikhodko.pk@gmail.com>
2025-02-04 19:34:02 +0300

    final fix

--- app/api/tests/check/[id]/route.ts
+++ app/api/tests/check/[id]/route.ts
@@ -3,20 +3,17 @@ import { testsTable } from '@/db/schema/tests-schema'
 import { eq } from 'drizzle-orm'
 import { NextRequest, NextResponse } from 'next/server'
 
-// Use the correct type from Next.js
-type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
 export async function GET(
   request: NextRequest,
-  props: Props
+
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params;
     const test = await db.query.tests.findFirst({
-      where: eq(testsTable.id, parseInt(props.params.id))
+      where: eq(testsTable.id, parseInt(id))
     })
+
 
     if (test) {
       return NextResponse.json({ status: 'completed' })
