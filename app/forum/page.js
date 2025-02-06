6f6a7c9b3f1226d0e16542343884fc1c208a5f12
[MotilQR] <[pantenkov08@bk.ru]>
2025-02-07 01:04:21 +0300

    ASda

--- app/forum/page.js
+++ app/forum/page.js
@@ -5,11 +5,11 @@ export default function Forum() {
 
 
     return (
-        <>
+        <div>
             <h3 className="font-bold max-w-2xl mx-auto p-4 text-xl mt-2">MAKE MY FORUM GREAT AGAIN</h3>
             <PostFeed
                 isPost={true}
             />
-        </>
+        </div>
     );
 }
